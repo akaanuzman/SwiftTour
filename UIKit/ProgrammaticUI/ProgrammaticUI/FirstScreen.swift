a79b4d2f4e5c60402436187724c7a3c970eea541
Kaan Uzman <55639112+akaanuzman@users.noreply.github.com>
2023-08-09 03:13:26 +0300

    uikit programmatic ui started

---
+++ UIKit/ProgrammaticUI/ProgrammaticUI/FirstScreen.swift
@@ -0,0 +1,50 @@
+//
+//  FirstScreen.swift
+//  ProgrammaticUI
+//
+//  Created by Kaan Uzman on 9/8/23.
+//
+
+import UIKit
+
+class FirstScreen: UIViewController {
+    
+    let nextButton = UIButton()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        setupButton()
+        title = "First Screen" // appbar title
+        navigationController?.navigationBar.prefersLargeTitles = true // appbar large title centerTitle false
+    }
+
+    func setupButton() -> Void {
+        view.addSubview(nextButton)
+        
+        nextButton.configuration = .tinted()
+        nextButton.configuration?.baseBackgroundColor = .systemOrange
+        nextButton.configuration?.baseForegroundColor = .systemOrange
+        nextButton.configuration?.title = "Next"
+        
+        nextButton.addTarget(self, action: #selector(goToNextScreen), for: .touchUpInside)
+        
+        nextButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor), // centering horizontally
+            nextButton.centerYAnchor.constraint(equalTo: view.centerYAnchor), // centering vertically
+            nextButton.widthAnchor.constraint(equalToConstant: 200),
+            nextButton.heightAnchor.constraint(equalToConstant: 50)
+            
+        ])
+    }
+    
+    @objc func goToNextScreen() -> Void {
+        let nextScreen = SecondScreen()
+        nextScreen.title = "Second Screen"
+        navigationController?.pushViewController(nextScreen, animated: true)
+    }
+
+}
+
---
+++ UIKit/ProgrammaticUI/ProgrammaticUI/SecondScreen.swift
@@ -0,0 +1,19 @@
+//
+//  SecondScreen.swift
+//  ProgrammaticUI
+//
+//  Created by Kaan Uzman on 9/8/23.
+//
+
+import UIKit
+
+class SecondScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // if you are using a navigation controller, you can set the some codes here.
+        // else this ui is crashing
+        view.backgroundColor = .systemBackground
+    }
+
+}
