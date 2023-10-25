680a9f2dc359b2fec16078dcadf98702ad1544e0
Kaan Uzman <55639112+akaanuzman@users.noreply.github.com>
2023-10-25 17:44:16 +0300

    init app

---
+++ UIKit/PodcastApp/PodcastApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PodcastApp
+//
+//  Created by Kaan Uzman on 25/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
